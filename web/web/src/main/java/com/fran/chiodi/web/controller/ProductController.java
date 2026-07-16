13042bf39f1cd1cead46f145e827d416aeb74b05
francisco-chiodi <franchiodi98@gmail.com>
2026-07-16 16:45:11 -0300

    advance ond the structure

--- web/web/src/main/java/com/fran/chiodi/web/controller/ProductController.java
+++ web/web/src/main/java/com/fran/chiodi/web/controller/ProductController.java
@@ -1,16 +1,12 @@
-package com.tuempresa.autogestionable.controller;//petition receiver from the web LAIN I MISS YOU
-
-import com.tuempresa.autogestionable.model.Producto;
+package com.fran.chiodi.web;
+import com.fran.chiodi.web.FrontPage; //entity
+import com.fran.chiodi.web.ProductRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;//activates the get for the products display
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController//marks the clase as web cvontroller to drive api rest rutes (JSON)
-@RequestMapping("/api/productos")//main url LAIN I MISS YOU, any petition that satarts with this router, goes to this controller lain.
-@CrossOrigin(origins = "http://localhost:4200") // Permite conexión con Angular
-public class ProductoController {
+@RequestMapping("/api/productos")//main url , any petition that satarts with this router, goes to this controller lain.
+@CrossOrigin(origins = "http://localhost:4200") //made it cors able
+public class ProductController {
 
--- web/web/src/main/java/com/fran/chiodi/web/repository/ProductRepo.java
+++ web/web/src/main/java/com/fran/chiodi/web/repository/ProductRepo.java
@@ -1,5 +1,5 @@
 package com.fran.chiodi.web;
 
-import com.tuempresa.autogestionable.model.Producto;//the repo need to know that data is is saving and retrieving
+import com.fran.chiodi.model.Product;//the repo need to know that data is is saving and retrieving
 import org.springframework.data.jpa.repository.JpaRepository;// provides db methods (crud power)
 import org.springframework.stereotype.Repository;//turns the file into a db acces component (spring bean)
